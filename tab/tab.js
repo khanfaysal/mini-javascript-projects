9c9c001aa6018e8a262393ee22c42cd5ce90cc60
khan faysal <mdfaysalkhancse@gmail.com>
2023-02-13 18:16:02 +0600

    tab logic done

--- tab/tab.js
+++ tab/tab.js
@@ -1,7 +1,23 @@
-const btn = document.querySelectorAll('.tab-btn');
+const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector('.about');
 const articles = document.querySelectorAll('.content');
 
 about.addEventListener('click', function (e) {
-    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+
+    if (id) {
+
+        // remove active from others button
+
+        btns.forEach(function (btn) {
+            btn.classList.remove('active');
+            e.target.classList.add('active')
+        });
+        // hide others article
+        articles.forEach(function (article) {
+            article.classList.remove('active')
+        })
+        const element = document.getElementById(id);
+        element.classList.add('active')
+    }
 });
