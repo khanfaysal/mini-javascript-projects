146554d286986b9726a929b1b5f107f6f97427d6
khan faysal <mdfaysalkhancse@gmail.com>
2023-02-15 22:32:27 +0600

    time implement

--- countdown-timer/countdownTimer.js
+++ countdown-timer/countdownTimer.js
@@ -26,3 +26,19 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 console.log(items);
+
+let futureDate = new Date();
+// futureDate.toLocaleDateString();
+console.log(futureDate);
+
+let weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+let month = futureDate.getMonth();
+month = months[month];
+const year = futureDate.getFullYear();
+let hours = futureDate.getHours()
+hours = Math.floor(timestamp / 1000 / 60 / 60);
+
+const minutes = futureDate.getMinutes();
+giveaway.textContent = `giveaway end on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}`
+console.log(year);
